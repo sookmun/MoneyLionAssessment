bce9797aac35952efacb124d7154d40a768d84ee
Tan Sook Mun <stan0111@student.monash.edu>
2023-03-11 14:34:48 +0800

    initial commit

---
+++ src/main/java/com/example/MoneyLionAssesment/MoneyLionAssesmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.MoneyLionAssesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyLionAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyLionAssesmentApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/MoneyLionAssesment/MoneyLionAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MoneyLionAssesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyLionAssesmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
